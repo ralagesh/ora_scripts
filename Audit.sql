985c567de0703db840bdf1383dff56fe523045fd
ralagesh <105965614+ralagesh@users.noreply.github.com>
2022-05-21 12:43:08 +0530

    Update Audit.sql

--- Audit.sql
+++ Audit.sql
@@ -1,3 +1,8 @@
 Object Level Auditing
 ----------------------
 select * from DBA_OBJ_AUDIT_OPTS;
+
+Privilege level Auditing
+--------------------------
+select * from SYSTEM_PRIVILEGE_MAP;
+select * from DBA_PRIV_AUDIT_OPTS;
